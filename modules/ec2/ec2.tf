6780f2a573b35810770b3cc4ea2b5cb2b0c3384e
Guido Giunchi <guidaccio@Mela.local>
2021-11-18 14:36:34 +0100

    Committing..

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -13,15 +13,27 @@ data "aws_ami" "zic_ami" {
 }
 
 
+resource "aws_eip" "zic_eip" {
+  instance = "${aws_instance.zic.id}"
+  vpc = true
+  lifecycle {
+    create_before_destroy = true
+  }
+
+        tags = {
+        Name = "${var.user-identifier}-zic_eip"
+    }
+}
+
 resource "aws_instance" "zic" {
   ami = "${data.aws_ami.zic_ami.id}"
   instance_type = "m5.xlarge"
   disable_api_termination = false
-  associate_public_ip_address = true
+  //associate_public_ip_address = true
   private_ip = "172.33.64.20"
   subnet_id = "${var.zic_subnet_01_id}"
-  vpc_security_group_ids = ["${var.sg_pub_zic_id}"]
-  key_name = "${var.ec2_key_name}"
+  vpc_security_group_ids  = ["${var.sg_pub_zic_id}"]
+  key_name                = "${var.ec2_key_name}"
   iam_instance_profile = "${var.user-identifier}-iam_role_zic"  // See zic.tf line: 7
 
       tags = {
